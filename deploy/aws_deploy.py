46db8e897b5d5be28eed78df1fe216c2e0be2fcd
Anup Jasani <anup.jasani@adlumin.com>
2024-04-17 15:35:51 -0400

    gh workflow test

--- deploy/aws_deploy.py
+++ deploy/aws_deploy.py
@@ -1,83 +1,83 @@
-import boto3
-import argparse
-import os
-from logger import Logger
+# import boto3
+# import argparse
+# import os
+# from logger import Logger
 
-deploy = False
-force = False
-logger = Logger.new("aws_deploy", stdout=True)
-
-
-def create_arg_parser():
-    parser = argparse.ArgumentParser(description="Deploy to AWS")
-    parser.add_argument("--action", type=str, help="Action to perform on AWS (create | update | delete)")
-    parser.add_argument("--list-and-update", action="store_true", help="List and update AWS resources")
-    parser.add_argument("--region", type=str, default="us-east-1", help="AWS Region")
-    parser.add_argument("--profile", type=str, default="default", help="AWS Profile")
-    parser.add_argument("--tag", type=str, default="bedtime_bot", help="Tag to use for resources")
-    parser.add_argument("--key-pair", type=str, default="channel-bot-key-pair", help="Key pair to use for resources")
-    parser.add_argument("--deploy", action="store_true", help="Deploy AWS resources")
-    parser.add_argument("--force", action="store_true", help="Force deploy AWS resources")
-
-    # TODO: add more arguments
-    return parser
+# deploy = False
+# force = False
+# logger = Logger.new("aws_deploy", stdout=True)
 
 
-def action_check(args):
-    if args.deploy:
-        global deploy
-        deploy = True
-    if args.force:
-        global force
-        force = True
+# def create_arg_parser():
+#     parser = argparse.ArgumentParser(description="Deploy to AWS")
+#     parser.add_argument("--action", type=str, help="Action to perform on AWS (create | update | delete)")
+#     parser.add_argument("--list-and-update", action="store_true", help="List and update AWS resources")
+#     parser.add_argument("--region", type=str, default="us-east-1", help="AWS Region")
+#     parser.add_argument("--profile", type=str, default="default", help="AWS Profile")
+#     parser.add_argument("--tag", type=str, default="bedtime_bot", help="Tag to use for resources")
+#     parser.add_argument("--key-pair", type=str, default="channel-bot-key-pair", help="Key pair to use for resources")
+#     parser.add_argument("--deploy", action="store_true", help="Deploy AWS resources")
+#     parser.add_argument("--force", action="store_true", help="Force deploy AWS resources")
+
+#     # TODO: add more arguments
+#     return parser
 
 
-def validate_args(args):
-    if args.action == "create":
-        logger.debug("create" + " TEST" if deploy else "")
-    elif args.action == "update":
-        logger.debug("update" + " TEST" if deploy else "")
-    elif args.action == "delete":
-        logger.debug("delete" + " TEST" if deploy else "")
-    elif args.list_and_update:
-        logger.debug("list_and_update")
-    else:
-        return False
-
-    return True
+# def action_check(args):
+#     if args.deploy:
+#         global deploy
+#         deploy = True
+#     if args.force:
+#         global force
+#         force = True
 
 
-def create_ec2_client(region, profile):
-    return boto3.client("ec2", region_name=region, profile_name=profile)
+# def validate_args(args):
+#     if args.action == "create":
+#         logger.debug("create" + " TEST" if deploy else "")
+#     elif args.action == "update":
+#         logger.debug("update" + " TEST" if deploy else "")
+#     elif args.action == "delete":
+#         logger.debug("delete" + " TEST" if deploy else "")
+#     elif args.list_and_update:
+#         logger.debug("list_and_update")
+#     else:
+#         return False
+
+#     return True
 
 
-def create_ec2_instance(region, profile, tag):
-    ec2 = create_ec2_client(region, profile)
-    instances = ec2.create_instances(
-        ImageId='ami-0c101f26f147fa7fd',
-        MinCount=1,
-        MaxCount=1,
-        InstanceType='t2.micro',
-        KeyName='ec2-keypair',
-        TagSpecifications=[
-            {
-                'ResourceType': 'instance',
-                'Tags': [
-                    {
-                        'Key': 'Name',
-                        'Value': tag
-                    },
-                ]
-            },
-        ])
-    # TODO: add keypair, name, and create sg
-    return instances
+# def create_ec2_client(region, profile):
+#     return boto3.client("ec2", region_name=region, profile_name=profile)
 
 
-if __name__ == "__main__":
-    arg_parser = create_arg_parser()
-    args = arg_parser.parse_args()
-    logger.debug(f"args action: {args}")
-    if not validate_args(args):
-        arg_parser.print_help()
-        exit(1)
+# def create_ec2_instance(region, profile, tag):
+#     ec2 = create_ec2_client(region, profile)
+#     instances = ec2.create_instances(
+#         ImageId='ami-0c101f26f147fa7fd',
+#         MinCount=1,
+#         MaxCount=1,
+#         InstanceType='t2.micro',
+#         KeyName='ec2-keypair',
+#         TagSpecifications=[
+#             {
+#                 'ResourceType': 'instance',
+#                 'Tags': [
+#                     {
+#                         'Key': 'Name',
+#                         'Value': tag
+#                     },
+#                 ]
+#             },
+#         ])
+#     # TODO: add keypair, name, and create sg
+#     return instances
+
+
+# if __name__ == "__main__":
+#     arg_parser = create_arg_parser()
+#     args = arg_parser.parse_args()
+#     logger.debug(f"args action: {args}")
+#     if not validate_args(args):
+#         arg_parser.print_help()
+#         exit(1)
